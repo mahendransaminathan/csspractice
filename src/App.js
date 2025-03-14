cbe111d9079964fcf18a1d22b3042583a805ecbc
Mahe Sami <mahendransamynathan@gmail.com>
2025-03-14 16:46:51 +0000

    Updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,44 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <label className='labelPractice'>SamplePractice</label>
+
+	<input type="text" className="name"/>
+
+	<select className="dropdown">
+	<option value="1">Red </option> 
+	</select>       
+
+	<ul className="List"> 
+	<li key="1">Large</li>
+	<li key="1">Large</li>
+	</ul> 
         
+	<table>
+	<tr>
+	<th>vkp </th>
+	<th>Tir </th>
+	<th>Pal </th> 
+	</tr>
+	<tr>
+	<td>1</td>
+	<td>2 </td>
+	<td>3 </td> 
+	</tr>
+	<tr>
+	<td>1</td>
+	<td>2 </td>
+	<td>3 </td> 
+	</tr>
+
+	<tr>
+	<td>1</td>
+	<td>2 </td>
+	<td>3 </td> 
+	</tr>
+
+	</table>
       </header>
     </div>
   );
