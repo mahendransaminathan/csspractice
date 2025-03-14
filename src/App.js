1f19814f39dc399349ca32d825a5be5624d8f951
Mahe Sami <mahendransamynathan@gmail.com>
2025-03-14 16:52:03 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -8,41 +8,27 @@ function App() {
 
         <label className='labelPractice'>SamplePractice</label>
 
-	<input type="text" className="name"/>
+        <input type="text" className="name"/>
 
-	<select className="dropdown">
-	<option value="1">Red </option> 
-	</select>       
+        <select className="dropdown">
+        <option value="1">Red </option> 
+        </select>       
 
-	<ul className="List"> 
-	<li key="1">Large</li>
-	<li key="1">Large</li>
+        <ul>
+	<li key ="vkp">NAMA </li>
+	<li key ="sse">SSE </li>
+	<li key ="RES">RES </li>
 	</ul> 
-        
+
 	<table>
-	<tr>
-	<th>vkp </th>
-	<th>Tir </th>
-	<th>Pal </th> 
+	<tr> <th>vkp </th> <th>cbe </th> <th>tub </th>
 	</tr>
-	<tr>
-	<td>1</td>
-	<td>2 </td>
-	<td>3 </td> 
+	<tr> <td>1 </td> <td>2 </td> <td>3 </td>
 	</tr>
-	<tr>
-	<td>1</td>
-	<td>2 </td>
-	<td>3 </td> 
+	<tr> <td>1 </td> <td>2 </td> <td>3 </td>
 	</tr>
-
-	<tr>
-	<td>1</td>
-	<td>2 </td>
-	<td>3 </td> 
-	</tr>
-
 	</table>
+        
       </header>
     </div>
   );
