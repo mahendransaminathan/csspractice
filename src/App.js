5c1ed01e9c390fc3d89856f03bb088628bba94ca
Mahe Sami <mahendransamynathan@gmail.com>
2025-03-15 13:02:03 +0000

    Displayed text box in center

--- src/App.js
+++ src/App.js
@@ -4,32 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <label className='labelPractice'>SamplePractice</label>
-
-        <input type="range" className="name"/>
-        
-
-        <select>
-          
-        </select>
-        <ul>
-	<li key ="vkp">NAMA </li>
-	<li key ="sse">SSE </li>
-	<li key ="RES">RES </li>
-	</ul> 
-
-	<table>
-	<tr> <th>vkp </th> <th>cbe </th> <th>tub </th>
-	</tr>
-	<tr> <td>1 </td> <td>2 </td> <td>3 </td>
-	</tr>
-	<tr> <td>1 </td> <td>2 </td> <td>3 </td>
-	</tr>
-	</table>
-        
-      </header>
+      <div className="flex-container">
+      <input type="text" className='name' />
+      </div>     
     </div>
   );
 }
